ee69ccc086048d30f818f931050d324a1f9685da
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-02 17:23:04 +0530

    " -> 0 of 4 passed. pylint: 5.45/10 "

---
+++ m4/Assignment-2/bob_counter.py
@@ -0,0 +1,16 @@
+'''Assume s is a string of lower case characters.
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl', then your program should print
+Number of times bob occurs is: 2'''
+def main():
+    """defining main Subset """
+    inp_vara = input("Enter a text: ")
+    #input string S
+    inp_varb = 'bob'
+    count_o = 0
+    for i in range(len(inp_vara)): 
+        if inp_varb == inp_vara[i:i+3]:
+            count_o = count_o + 1
+    print(str(count_o))
+if __name__ == "__main__":
+    main()
---
+++ m4/p1/vowels_counter.py
@@ -0,0 +1,21 @@
+""""#Assume s is a string of lower case characters.
+#Write a program that counts up the number of vowels contained in the string s.
+ Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example,
+ if s = 'azcbobobegghakl', your program should print:
+#Number of vowels: 5
+"""
+def main():
+    """
+    main function
+    """
+    inp_var = raw_input("")
+    vowels_str = ""
+    consonents_str = ""
+    for char in inp_var:
+        if char in 'AEIOUaeiou':
+            vowels_str += char
+        else:
+            consonents_str += char
+    print(len(vowels_str))
+if __name__ == "__main__":
+    main()
