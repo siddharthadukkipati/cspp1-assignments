714bffe31eb524495ae48520f454ae7e9b22eec7
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-06 19:19:35 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- m7/Functions - Assignment-2/assignment2.py
+++ m7/Functions - Assignment-2/assignment2.py
@@ -1,5 +1,5 @@
 """
-Paying Debt off in a Year
+Assignment-2 - Paying Debt off in a Year
 """
 def paying_debt_off_in_a_year(inp_balance, annual_interest_rate):
     """# Assume that the interest is compounded monthly according to the balance
@@ -18,20 +18,20 @@ def paying_debt_off_in_a_year(inp_balance, annual_interest_rate):
         balance = inp_balance
         monthly_payment = monthly_payment + 10
         month = 1
-        while month < 12:
-            monthly_interest_rate = (annual_interest_rate) // 12.0
+        while month <= 12:
+            monthly_interest_rate = (annual_interest_rate) / 12.0
             monthly_unpaid_balance = (balance) - (monthly_payment)
-            updated_balance_each_month = (monthly_unpaid_balance) * \
+            updated_balance_each_month = (monthly_unpaid_balance) + \
             (monthly_interest_rate * monthly_unpaid_balance)
             balance = updated_balance_each_month
-            month = 1
-    return monthly_payment
+            month += 1
+    return monthy_payment
 def main():
     """ main function"""
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: " + str(paying_debt_off_in_a_year(data[0], data[1])))
+    print("Lowest Payment: " + str(paying_debt_off_in_a_year(data[0], data[1]))
 if __name__ == "__main__":
     main()
 
