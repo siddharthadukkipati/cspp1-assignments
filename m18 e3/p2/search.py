0ef68556e2d0b8203963541b8a3a42ff38f9c00c
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-20 18:27:38 +0530

    " -> 1 of 1 passed. pylint: 9.00/10 "

--- m18 e3/p2/search.py
+++ m18 e3/p2/search.py
@@ -37,7 +37,7 @@ def search(search_index, query):
         collect all the values for the words that are in the search_index
         make a set of doc_id and return
     '''
-    query = query.lower().split('')
+    query = query.lower().split(' ')
     doc_id = []
     for word in query:
         if word in search_index:
