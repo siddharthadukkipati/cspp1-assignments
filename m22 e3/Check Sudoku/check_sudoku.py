6438af6ee54d0c3e1bfa83b596b6291f9c70ac33
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-25 13:24:05 +0530

    " -> 0 of 26 passed. pylint: 5.68/10 "

--- m22 e3/Check Sudoku/check_sudoku.py
+++ m22 e3/Check Sudoku/check_sudoku.py
@@ -55,7 +55,7 @@ def main():
     '''
         main function to read input sudoku from console
         call check_sudoku function and print the result to console
-    '''    
+    '''
     # initialize empty list
     sudoku = []
 
