1fd1f59983d108da6ac3072dd05ca837380f01db
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-25 13:17:25 +0530

    " -> 0 of 26 passed. pylint: 3.64/10 "

--- m22 e3/Check Sudoku/check_sudoku.py
+++ m22 e3/Check Sudoku/check_sudoku.py
@@ -13,42 +13,43 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
+    sudoku = list_12
     for i in range(9):
-        l = lst[i]
-        if "".join(l.sort()) != "123456789":
+        lis_1 = list_12[i]
+        if "".join(lis_1.sort()) != "123456789":
             return False
     for i in range(9):
-        l = []
+        lis_1 = []
         for j in range(9):
-            l.append(lst[j][i])
-        if "".join(sorted(l)) != "123456789":
+            lis_1.append(list_12[j][i])
+        if "".join(sorted(lis_1)) != "123456789":
             return False
 
-    if "".join(sorted(lst[0][0]+lst[0][1]+lst[0][2]+ lst[1][0]+ lst[1][1]+lst[1][2]+lst[2][0]+lst[2][1]+lst[2][2])) != "123456789":
+    if "".join(sorted(list_12[0][0]+list_12[0][1]+list_12[0][2]+ list_12[1][0]+ list_12[1][1]+list_12[1][2]+list_12[2][0]+list_12[2][1]+list_12[2][2])) != "123456789":
         return False
-    if "".join(sorted(lst[0][3]+lst[0][4]+lst[0][5]+ lst[1][3]+ lst[1][4]+lst[1][5]+lst[2][3]+lst[2][4]+lst[2][5])) != "123456789":
+    if "".join(sorted(list_12[0][3]+list_12[0][4]+list_12[0][5]+ list_12[1][3]+ list_12[1][4]+list_12[1][5]+list_12[2][3]+list_12[2][4]+list_12[2][5])) != "123456789":
         return False  
-    if "".join(sorted(lst[0][6]+lst[0][7]+lst[0][8]+ lst[1][6]+ lst[1][7]+lst[1][8]+lst[2][6]+lst[2][7]+lst[2][8])) != "123456789":
+    if "".join(sorted(list_12[0][6]+list_12[0][7]+list_12[0][8]+ list_12[1][6]+ list_12[1][7]+list_12[1][8]+list_12[2][6]+list_12[2][7]+list_12[2][8])) != "123456789":
         return False 
-    if "".join(sorted(lst[3][0]+lst[3][1]+lst[3][2]+ lst[4][0]+ lst[4][1]+lst[4][2]+lst[5][0]+lst[5][1]+lst[5][2])) != "123456789":
+    if "".join(sorted(list_12[3][0]+list_12[3][1]+list_12[3][2]+ list_12[4][0]+ list_12[4][1]+list_12[4][2]+list_12[5][0]+list_12[5][1]+list_12[5][2])) != "123456789":
         return False
-    if "".join(sorted(lst[3][3]+lst[3][4]+lst[3][5]+ lst[4][3]+ lst[4][4]+lst[4][5]+lst[5][3]+lst[5][4]+lst[5][5])) != "123456789":
+    if "".join(sorted(list_12[3][3]+list_12[3][4]+list_12[3][5]+ list_12[4][3]+ list_12[4][4]+list_12[4][5]+list_12[5][3]+list_12[5][4]+list_12[5][5])) != "123456789":
         return False  
-    if "".join(sorted(lst[3][6]+lst[3][7]+lst[3][8]+ lst[4][6]+ lst[4][7]+lst[4][8]+lst[5][6]+lst[5][7]+lst[5][8])) != "123456789":
+    if "".join(sorted(list_12[3][6]+list_12[3][7]+list_12[3][8]+ list_12[4][6]+ list_12[4][7]+list_12[4][8]+list_12[5][6]+list_12[5][7]+list_12[5][8])) != "123456789":
         return False
-    if "".join(sorted(lst[6][0]+lst[6][1]+lst[6][2]+ lst[7][0]+ lst[7][1]+lst[7][2]+lst[8][0]+lst[8][1]+lst[8][2])) != "123456789":
+    if "".join(sorted(list_12[6][0]+list_12[6][1]+list_12[6][2]+ list_12[7][0]+ list_12[7][1]+list_12[7][2]+list_12[8][0]+list_12[8][1]+list_12[8][2])) != "123456789":
         return False
-    if "".join(sorted(lst[6][3]+lst[6][4]+lst[6][5]+ lst[7][3]+ lst[7][4]+lst[7][5]+lst[8][3]+lst[8][4]+lst[8][5])) != "123456789":
+    if "".join(sorted(list_12[6][3]+list_12[6][4]+list_12[6][5]+ list_12[7][3]+ list_12[7][4]+list_12[7][5]+list_12[8][3]+list_12[8][4]+list_12[8][5])) != "123456789":
         return False
-    if "".join(sorted(lst[6][6]+lst[6][7]+lst[6][8]+ lst[7][6]+ lst[7][7]+lst[7][8]+lst[8][6]+lst[8][7]+lst[8][8])) != "123456789":
+    if "".join(sorted(list_12[6][6]+list_12[6][7]+list_12[6][8]+ list_12[7][6]+ list_12[7][7]+list_12[7][8]+list_12[8][6]+list_12[8][7]+list_12[8][8])) != "123456789":
         return False
     return True
 
-    n = 9
-    lst = []
-    for _ in range(n):
-        lst.append(input().split())
-    print(check(lst))
+    num_n = 9
+    list_12 = []
+    for _ in range(num_n):
+        list_12.append(input().split())
+    print(check(list_12))
 
 def main():
     '''
