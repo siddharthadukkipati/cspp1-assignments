ea1b18818e88223ae5233256e0699f09258836bf
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-27 10:06:35 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- m4/p2/bob_counter.py
+++ m4/p2/bob_counter.py
@@ -4,10 +4,10 @@ number of times bob occurs is: 2'''
 def main():
     """definition"""
     count_v = 0
-    s_v = input()
-    s_v2 = 'bob'
-    for i in range(len(s_v)):
-        if s_v2 == s_v[i:i+3]:
+    inp_str = input()
+    comp_str = 'bob'
+    for i in range(len(inp_str)):
+        if comp_str == inp_str[i:i+3]:
             count_v = count_v + 1
     print(count_v)
 if __name__ == "__main__":
