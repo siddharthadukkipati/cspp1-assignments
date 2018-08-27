547064f30a19a2cce027fc3b50b623137ee936b4
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-27 10:08:14 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- m4/p3/longest_substring.py
+++ m4/p3/longest_substring.py
@@ -4,18 +4,18 @@ of s in which the letters occur in alphabetical order'''
 def main():
     """defining main function"""
     str_s = input()
-    m_c = 0
-    g_c = 0
-    c_co = 0
+    max_count = 0
+    gain_count = 0
+    count_count = 0
     for i in range(len(str_s)-1):
         if str_s[i] <= str_s[i+1]:
-            c_co += 1
+            count_count += 1
         else:
-            c_co = 0
-        if g_c < c_co:
-            g_c = c_co
-            m_c = i
-    d_d = m_c-g_c+1
-    print(str_s[d_d:d_d+g_c+1])
+            count_count = 0
+        if gain_count < count_count:
+            gain_count = count_count
+            max_count = i
+    d_d = max_count-gain_count+1
+    print(str_s[d_d:d_d+gain_count+1])
 if __name__ == "__main__":
     main()
